8f51ea2eea31b6219c38eb27897bc98375945673
DEMIDOB <5OxymA16TytlikuGIT>
2023-10-06 22:51:33 +0200

    something like fireplace

--- particle.js
+++ particle.js
@@ -1,5 +1,5 @@
 const xVelRad = 1;
-const minYVel = 0.2, maxYVel = 10;
+const minYVel = 0.01, maxYVel = 10;
 
 class Particle {
     constructor(initialPos = null) {
@@ -23,13 +23,14 @@ class Particle {
         }
 
         if (!acc) {
-            acc = createVector(0, 0);
+            acc = createVector(random(-xVelRad, xVelRad) * 0.5, 0);
+            acc = createVector();
         }
 
         this.vel.add(acc);
         this.pos.add(this.vel);
 
-        if (this.pos.x < 0 || this.pos.y < 0) {
+        if (this.pos.x < 0 || this.pos.y < 0 || this.pos.x > width || this.pos.y > height) {
             this.initialized = false;
         }
     }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-const PARTICLES_AMOUNT = 100;
-const STEP = 10;
-const NOISE_STEP = 75;
+const PARTICLES_AMOUNT = 256;
+let step = 1;
+let NOISE_STEP = 20;
 
 let particles = [];
 let currentX = 0;
@@ -13,6 +13,8 @@ function setup() {
     // createCanvas(windowWidth, windowHeight);
     createCanvas(400, 400);
     // frameRate(1);
+    step = width / 25;
+    // NOISE_STEP = width / 20;
 
     background(0);
 
@@ -22,25 +24,58 @@ function setup() {
 }
 
 function draw() {
-    background(0, 100);
+    background(0, 50);
 
     let noiseHeight = height / 2;
-
-    for (let p of particles) {
-        p.update();
-        p.draw(color(255, 200 * (height * 1.3 - p.pos.y) / height, 0, 255 * p.pos.y / height));
-    }
     nextParticle = getNextParticle();
     while (mouseIsPressed && nextParticle && mouseX > 0 && mouseX < width) {
         nextParticle.reinit(createVector(mouseX, mouseY));
         nextParticle = getNextParticle();
     }
 
-    for (let x = 0; x < width; x += STEP) {
-
+    let lastStep;
+    for (let s = step; s < width; s *= 1.1) {
+        lastStep = s;
     }
 
-    batch += 0.1;
+    for (let p of particles) {
+        p.update();
+        p.draw(color(255, 200 * (height * 1.3 - p.pos.y) / height, 0, 255 * (p.pos.y - height / 4) / height));
+    }
+
+    for (let s = step; s < width; s *= 1.1) {
+        noStroke();
+        fill(175,  100 * width / step / s, 0);
+        beginShape();
+        vertex(0, height);
+
+        let ps = float("-inf");
+        let pps = float("-inf");
+
+        for (let x = 10; x < width; x += s) {
+            let y = height - noise(x / NOISE_STEP, batch) * width / step / s * (noiseHeight - 2/3 * noiseHeight * 2 * abs(x - width / 2) / width);
+
+            if (s - lastStep / (1.1 ** 10) < 1) {
+
+                if (ps > pps && ps > y) {
+                    nextParticle = getNextParticle();
+                    if (nextParticle) {
+                        nextParticle.reinit(createVector(x, y));
+                    }
+                }
+
+                pps = ps;
+                ps = y;
+            }
+
+            vertex(x, y);
+        }
+        vertex(width, height);
+        endShape();
+        // break;
+    }
+
+    batch += 0.05;
 }
 
 function getNextParticle() {
